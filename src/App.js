6192817a72d8efa57d58afbb55eee3d155283eb5
stuart-p <stuart_palmer85@hotmail.com>
2020-03-11 11:43:29 +0000

    game object displays on the login page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import Header from "./components/Header-components/Header";
 import Login from "./components/login-components/Login";
 import Lobby from "./components/Lobby-components/Lobby";
 import socketIOClient from "socket.io-client";
+//temp for testing initial game functionality
+import { IonPhaser } from "@ion-phaser/react";
+import { gameSceneConfig, passSocketRef } from "./scenes/gameScene";
 const socket = socketIOClient("localhost:8080");
 
 class App extends React.Component {
-  state = { loggedIn: false, userList: null };
+  state = {
+    loggedIn: false,
+    userList: null,
+    initialize: true,
+    game: gameSceneConfig
+  };
 
-  componentDidMount() {}
+  componentDidMount() {
+    passSocketRef(socket);
+  }
 
   render() {
     return (
@@ -29,6 +39,10 @@ class App extends React.Component {
             />
             <QuizPage path="/quiz" users={this.state.users} socket={socket} />
           </Router>
+          <IonPhaser
+            game={this.state.game}
+            initialize={this.state.initialize}
+          />
         </body>
       </div>
     );
---
+++ src/scenes/gameScene.js
@@ -0,0 +1,308 @@
+import Phaser from "phaser";
+import socketIOClient from "socket.io-client";
+
+const passSocketRef = socket => {
+  clientSocket = socket;
+};
+
+const gameSceneConfig = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+      gravity: { y: 0 }
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+let dolly;
+let life = undefined;
+let spacebar;
+let qu;
+let wu;
+let ee;
+let clientSocket;
+
+function preload() {
+  this.load.image("genie", "assets/10.png");
+  this.load.image("baddie", "assets/13.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("tiles", "assets/rogue.png");
+  this.load.tilemapTiledJSON("map", "assets/mapTest.json");
+  this.load.image("fireball", "assets/spell.png");
+  this.load.image("life2", "assets/2.png");
+  this.load.image("life1", "assets/1.png");
+  // this.load.image("life0", "assets/0.png");
+  this.load.image("life3", "assets/3.png");
+  this.load.image("firering", "assets/firering.png");
+  this.load.image("dead", "assets/bomb.png");
+  this.load.image("green", "assets/green.png");
+  this.load.image("red", "assets/red.png");
+}
+
+function create() {
+  const self = this;
+  this.socket = socketIOClient("localhost:8080");
+  this.players = this.add.group();
+  this.attacks = this.add.group();
+  this.stats = this.add.group();
+  this.something = this.add.group();
+  dolly = this.physics.add.image(100, 100, "star");
+  this.cameras.main.setDeadzone(50, 50);
+  this.cameras.main.startFollow(dolly, true, 0.05, 0.05);
+  this.cameras.main.setZoom(1.6);
+
+  this.socket.on("currentPlayers", players => {
+    Object.keys(players).forEach(id => {
+      if (players[id].playerID === self.socket.id) {
+        displayPlayers(self, players[id], "genie");
+      } else {
+        displayPlayers(self, players[id], "baddie");
+      }
+    });
+  });
+
+  this.socket.on("newPlayer", playerInfo => {
+    displayPlayers(self, playerInfo, "baddie");
+  });
+  this.socket.on("newAttack", playerInfo => {
+    displayAttacks(self, playerInfo);
+  });
+
+  this.socket.on("disconnect", playerID => {
+    self.players.getChildren().forEach(player => {
+      if (playerID === player.playerID) {
+        player.destroy();
+      }
+    });
+  });
+
+  this.socket.on("attackEnded", attackID => {
+    self.attacks.getChildren().forEach(attack => {
+      if (attackID === attack.attackID) {
+        attack.destroy();
+      }
+    });
+  });
+
+  this.socket.on("somethingAdded", somethingInfo => {
+    console.log("thingthing", somethingInfo.thing);
+    showSomething(self, somethingInfo.player, somethingInfo.thing);
+  });
+
+  this.socket.on("playerUpdates", players => {
+    // if (players[this.socket.id].life !== life) {
+    if (players[this.socket.id] !== undefined) {
+      if (life === undefined) {
+        life = players[this.socket.id].life;
+        displayLife(self, players[this.socket.id]);
+      } else if (players[this.socket.id].life !== life) {
+        self.stats.getChildren().forEach(stat => {
+          stat.destroy();
+        });
+        life = players[this.socket.id].life;
+        displayLife(self, players[this.socket.id]);
+      } else {
+        self.stats.getChildren().forEach(stat => {
+          // console.log(id);
+          stat.setPosition(
+            players[this.socket.id].x,
+            players[this.socket.id].y + 100
+          );
+        });
+      }
+      // this.stats[this.socket.id].x = players[this.socket.id].x + 190;
+      // this.stats[this.socket.id].y = players[this.socket.id].y - 150;
+    }
+    Object.keys(players).forEach(id => {
+      self.players.getChildren().forEach(player => {
+        if (players[id].playerID === player.playerID) {
+          player.setRotation(players[id].rotation);
+          player.setPosition(players[id].x, players[id].y);
+        }
+      });
+      self.players.getChildren().forEach(player => {
+        if (players[player.playerID] === undefined) {
+          player.destroy();
+          console.log("destroyed");
+        }
+      });
+    });
+    // self.players.getChildren().forEach(player => {
+    //   console.log(player.playerID);
+    //   if (players[player.playerID] === undefined) {
+    //     player.destroy();
+    //   }
+    // });
+    if (players[this.socket.id])
+      dolly.setPosition(players[this.socket.id].x, players[this.socket.id].y);
+  });
+
+  this.socket.on("somethingUpdates", data => {
+    self.something.getChildren().forEach(something => {
+      if (data.somethings[something.somethingID] === undefined) {
+        something.destroy();
+      }
+    });
+    Object.keys(data.somethings).forEach(id => {
+      self.something.getChildren().forEach(something => {
+        if (something.somethingID === id) {
+          something.setPosition(data.players[id].x, data.players[id].y);
+        }
+      });
+    });
+  });
+
+  this.socket.on("attackUpdates", attacks => {
+    Object.keys(attacks).forEach(id => {
+      self.attacks.getChildren().forEach(attack => {
+        if (attacks[id].attackID === attack.attackID) {
+          attack.setPosition(attacks[id].x, attacks[id].y);
+        }
+      });
+    });
+    self.attacks.getChildren().forEach(attack => {
+      if (attacks[attack.attackID] === undefined) {
+        attack.destroy();
+      }
+    });
+  });
+
+  this.socket.on("onDie", playerID => {
+    self.players.getChildren().forEach(player => {
+      if (player.playerID === playerID) {
+        self.add.image(player.x, player.y, "dead");
+        player.setTexture("star");
+      }
+    });
+  });
+
+  this.cursors = this.input.keyboard.createCursorKeys();
+  spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  qu = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+  wu = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  ee = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+  this.leftKeyPressed = false;
+  this.rightKeyPressed = false;
+  this.upKeyPressed = false;
+  this.downKeyPressed = false;
+
+  const map = this.make.tilemap({ key: "map" });
+
+  const tileset = map.addTilesetImage("rogue", "tiles");
+  const layerOne = map.createStaticLayer("floor", tileset, 0, 0);
+  const layerTwo = map.createStaticLayer("walls", tileset, 0, 0);
+}
+
+function update() {
+  const left = this.leftKeyPressed;
+  const right = this.rightKeyPressed;
+  const up = this.upKeyPressed;
+  const down = this.downKeyPressed;
+
+  if (this.cursors.left.isDown) {
+    this.leftKeyPressed = true;
+    this.rightKeyPressed = false;
+  } else if (this.cursors.right.isDown) {
+    this.rightKeyPressed = true;
+    this.leftKeyPressed = false;
+  } else {
+    this.leftKeyPressed = false;
+    this.rightKeyPressed = false;
+  }
+
+  if (this.cursors.up.isDown) {
+    this.upKeyPressed = true;
+    this.downKeyPressed = false;
+  } else if (this.cursors.down.isDown) {
+    this.downKeyPressed = true;
+    this.upKeyPressed = false;
+  } else {
+    this.downKeyPressed = false;
+    this.upKeyPressed = false;
+  }
+
+  if (
+    left !== this.leftKeyPressed ||
+    right !== this.rightKeyPressed ||
+    up !== this.upKeyPressed ||
+    down !== this.downKeyPressed
+  ) {
+    this.socket.emit("playerInput", {
+      left: this.leftKeyPressed,
+      right: this.rightKeyPressed,
+      up: this.upKeyPressed,
+      down: this.downKeyPressed
+    });
+  }
+  // if (this.cursors.space.JustDown) {
+  if (Phaser.Input.Keyboard.JustDown(spacebar)) {
+    console.log("SHOOTING!!!");
+    this.socket.emit("attackInput", "hi");
+  }
+  if (Phaser.Input.Keyboard.JustDown(qu)) {
+    console.log("SHOOTING!!!");
+    this.socket.emit("attackInput", "hi");
+  }
+  if (Phaser.Input.Keyboard.JustDown(wu)) {
+    console.log("something!!!");
+    this.socket.emit("something", "firering");
+  }
+}
+
+function displayPlayers(self, playerInfo, sprite) {
+  const player = self.add
+    .sprite(playerInfo.x, playerInfo.y, sprite)
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(50, 50);
+  // if (playerInfo.team === "blue") {
+  //   // player.setTint(0x0000ff);
+  // } else {
+  //   // player.setTint(0xff0000);
+  // }
+
+  player.playerID = playerInfo.playerID;
+  self.players.add(player);
+}
+function displayAttacks(self, playerInfo) {
+  const attack = self.add
+    .sprite(playerInfo.x, playerInfo.y, "fireball")
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(50, 50);
+
+  attack.attackID = playerInfo.attackID;
+  self.attacks.add(attack);
+}
+
+function displayLife(self, player) {
+  const myLife = self.add
+    .sprite(player.x, player.y + 100, "red")
+    .setDisplaySize(25, 25);
+  myLife.statID = player.playerID;
+  self.stats.add(myLife);
+  const myCurrentLife = self.add
+    .image(player.x, player.y + 100, "green")
+    .setDisplaySize(25, 25);
+  myCurrentLife.statID = player.playerID + 1;
+  myCurrentLife.scaleX = player.life / 3;
+  self.stats.add(myCurrentLife);
+}
+
+function showSomething(self, player, sprite) {
+  const mySomething = self.add
+    .sprite(player.x, player.y, sprite)
+    .setDisplaySize(125, 125);
+  mySomething.somethingID = player.playerID;
+  self.something.add(mySomething);
+}
+
+export { gameSceneConfig, passSocketRef };
