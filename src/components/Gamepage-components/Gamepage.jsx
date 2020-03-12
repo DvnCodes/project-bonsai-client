66d072b1343137e04256a7b2f187a24b9ed43d77
Jayfromp <jcompson@hotmail.co.uk>
2020-03-12 15:12:44 +0000

    create banished

--- src/components/Gamepage-components/Gamepage.jsx
+++ src/components/Gamepage-components/Gamepage.jsx
@@ -7,13 +7,16 @@ class Gamepage extends Component {
     initialize: true,
     game: gameSceneConfig,
     endpoint: "https://projects-game-backend.herokuapp.com/",
-    socket: this.props.socket
+    socket: this.props.socket,
+    isBanished: false
   };
   render() {
+    const { isBanished } = this.state;
     return (
       <div>
         <h1>GAMEPAGE</h1>
         <IonPhaser game={this.state.game} initialize={this.state.initialize} />
+        {isBanished && <h1>BANISHED</h1>}
       </div>
     );
   }
@@ -21,6 +24,25 @@ class Gamepage extends Component {
   componentDidMount() {
     this.setState({ socket: this.props.socket });
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state !== prevState) {
+      this.state.socket.on("onDie", playerId => {
+        if (playerId === this.state.socket.id) {
+          this.setState({ isBanished: true });
+        }
+      });
+    }
+  }
 }
+
+/*
+onDie listener emits player.playerID
+all clients listen for banished, if the socket.id is the same as player, then
+have a listener in the gamepage, which sets isBanished: true is state
+if isBanished:true - a func component is rendered
+func component renders a <h1> 'Banished' </h1> which transitions on screen over the game.
+
+*/
 
 export default Gamepage;
