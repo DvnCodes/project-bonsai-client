58a7f087b7e3b560341ffb367cb4077353e42188
jamie <jamiegormley1987@outlook.com>
2020-03-12 16:28:15 +0000

    stats page laods

--- src/components/Gamepage-components/GameSummary.jsx
+++ src/components/Gamepage-components/GameSummary.jsx
@@ -1,33 +1,45 @@
 import React, { Component } from "react";
 
 class GameSummary extends Component {
-  state = { statsData: null };
-  componentDidMount() {
-    console.log(this.props.statsData[this.props.socket.id].username);
-  }
+  state = { statsData: this.props.statsData };
 
   render() {
     return (
       <div>
         <h1>Game Summary</h1>
-        {this.state.statsData && <h2>test</h2>}
-        {this.props.statsData && (
-          <>
-            {Object.keys(this.props.statsData).forEach(ID => {
-              if (this.props.statsData[ID].winner === true) {
-                return <h2>Winner: {this.props.statsData[ID].username} </h2>;
-              }
-            })}
-          </>
-        )}
-        {/* <p>{this.props.statsData[0].life}</p> */}
+
+        {Object.keys(this.state.statsData).map(ID => {
+          console.log(this.state.statsData[ID].winner);
+          if (this.state.statsData[ID].winner === true) {
+            return <h2>WINNER: {this.state.statsData[ID].username}</h2>;
+          }
+        })}
+
+        <ul>
+          {Object.keys(this.state.statsData).map(ID => {
+            return (
+              <li>
+                {this.state.statsData[ID].username} : KILLS:{" "}
+                {this.state.statsData[ID].kills} HITS:{" "}
+                {this.state.statsData[ID].hits}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.statsData === undefined && this.props.statsData) {
+    if (
+      prevProps.statsData === undefined &&
+      this.props.statsData !== undefined
+    ) {
       this.setState({ statsData: this.props.statsData });
+      console.log("gamedata added to state");
+    } else if (this.props.statsData !== prevProps.statsData) {
+      this.setState({ statsData: this.props.statsData });
+      console.log("gamedata added to state");
     }
   }
 }
