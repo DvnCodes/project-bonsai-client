fb1e90f34a60d9fc9250272ddbe66d6d91a044ff
stuart-p <stuart_palmer85@hotmail.com>
2020-03-17 12:18:25 +0000

    added min-max username validation. modified standard button style

--- src/components/Header-components/Header.jsx
+++ src/components/Header-components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { HeaderContainer } from "../Styles/container.styles";
 
-const Header = () => {
+const Header = props => {
   return (
     <HeaderContainer>
       <h1>Dungeon Mathsters</h1>
+      <button onClick={props.toggleAudio}>toggle audio</button>
     </HeaderContainer>
   );
 };
